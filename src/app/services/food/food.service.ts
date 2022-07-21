26c0fba06e834b1d4b3eefa599e09e5a7ceb6e6f
NoobDeveloper412 <74310120+NoobDeveloper412@users.noreply.github.com>
2022-07-21 17:08:19 +0500

    Fetched all the food images and then showed them in the Home Component.

--- src/app/services/food/food.service.ts
+++ src/app/services/food/food.service.ts
@@ -6,4 +6,12 @@ import { Injectable } from '@angular/core';
 export class FoodService {
 
   constructor() { }
+
+  getFoodList(): string[] {
+    return [
+      '/assets/img (1).jpg',
+      '/assets/img (2).jpg',
+      '/assets/img (3).jpg'
+    ]
+  }
 }
